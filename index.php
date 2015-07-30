93920efebb67eafa97e9eea2683f8d393edc1df3
Chinmay <chinmay235@gmail.com>
2015-07-30 11:29:21 +0530

    FPDF Password Protection

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require('fpdf_protection.php');
+
+$pdf=new FPDF_Protection();
+$pdf->SetProtection(array('print'),'password');
+//$pdf->SetProtection(array('print'));
+$pdf->AddPage();
+$pdf->SetFont('Arial');
+$pdf->Write(10,'You can print me but not copy my text.');
+$pdf->Output();
+?>
